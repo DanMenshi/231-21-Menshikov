7dd2eecdc33a041c9ee36889b12c01d86f7625d7
DanMenshi <danissimo02032007@gmail.com>
2025-02-26 13:31:21 +0300

    Update wp-config.php

--- project 01/menshikov/wp-config.php
+++ project 01/menshikov/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'menshikov231' );
+define( 'DB_NAME', 'menshikov' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
